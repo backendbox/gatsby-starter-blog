5ad08089ff998146675ed69f6c8b3922666cfda1
Truong Vo <truongvo@Truongs-MacBook-Pro.local>
2019-01-31 22:32:43 +0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Ken's Blog`,
+    author: `Ken Vo Nam Truong`,
+    description: `My thoughts`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
